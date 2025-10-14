5f9813bf810e62f86f1995cd0ff15d763df0eec0
Gabriel Volpe <volpegabriel@gmail.com>
2025-10-14 20:10:17 +0200

    delete shell script

--- src/drv.nix
+++ src/drv.nix
@@ -1,19 +1,22 @@
 { lib, python3Packages }:
 
+let
+  binname = "nscratch";
+in
 python3Packages.buildPythonApplication {
   pname = "niri-scratchpad";
   version = "0.0.1";
   pyproject = false;
   propagatedBuildInputs = [ ];
   dontUnpack = true;
-  installPhase = "install -Dm755 ${./ns.py} $out/bin/nscratch";
+  installPhase = "install -Dm755 ${./ns.py} $out/bin/${binname}";
 
   meta = {
     description = "Scratchpad support for the Niri Wayland compositor";
     homepage = "https://github.com/gvolpe/niri-scratchpad";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ gvolpe ];
-    mainProgram = "nscratch";
+    mainProgram = binname;
     platforms = lib.platforms.linux;
   };
 }
